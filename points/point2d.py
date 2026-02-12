080a3d411ca30a2b84900de4cf9f279884b2b3c2
joamona <joamona@cgf.upv.es>
2026-02-12 17:40:22 +0100

    m

--- points/point2d.py
+++ points/point2d.py
@@ -10,26 +10,29 @@ class Point2D:
         self.setY(y)
 
     #setter--> Comprueban datos de entrada
-    def setX(self,x):
+    def setX(self,x)->float:
         self.x=self._checkValue(x)
+        return self.x
     #setter
-    def setY(self,y):
+    def setY(self,y)->float:
         self.y=self._checkValue(y)
-
+        return self.y
+    
     def printCoordinates(self):
         a=10 #variables locales. Solo viven aquí
         b=20
         c=a+b
         print(f"({self.x}, {self.y})")
 
-    def translate(self, dx, dy):
+    def translate(self, dx, dy)->list:
         self.x =self.x + dx
         self.y = self.y + dy
+        return self.getAsList()
     
-    def getAsList(self):
+    def getAsList(self)->list:
         return [self.x, self.y]
     
-    def getAsDict(self):
+    def getAsDict(self)->dict:
         return {'x':self.x, 'y': self.y}
     
     
@@ -48,6 +51,19 @@ class Point3D(Point2D):
         self.z=None
         super().__init__(x, y)
         self.z=self._checkValue(z)
+    def getAsList(self):
+        l = super().getAsList()
+        l.append(self.z)
+        return l
+        
+    def getAsDict(self):
+        d=super().getAsDict()
+        d['z']=self.z
+        return d
+        return {**super().getAsDict(),'z':self.z}
+        
 
 if __name__=="__main__":
-    p=Point3D(1,2,3)   
+    p=Point3D(1,2,3) 
+    print(p.getAsDict())  
+    print(p.getAsList())  
